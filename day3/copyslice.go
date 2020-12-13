fe62f7793077414cc127cea1a7037cbfdb4339d5
gowrisankar <gowrisankar.m01@sap.com>
2020-12-13 22:21:36 +0530

    init

--- day3/copyslice.go
+++ day3/copyslice.go
@@ -6,7 +6,7 @@ func main()  {
 
 	a := []int{1,2,3,4}
 
-	b := make([]int, 3)
+	b := make([]int, len(a))
 
 	copy(b,a) // copy will copy only based on the length on make cases.
 
---
+++ filefinder/filefinder.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main()  {
+
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		log.Println("Please enter the directory name")
+		return
+	}
+
+	readdir, err := ioutil.ReadDir(args[0])
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	var filesss []byte
+
+	for _,v := range readdir{
+		if v.Size() == 0 {
+			filesss = append(filesss,v.Name()...)
+		}
+		err = ioutil.WriteFile("hello.txt",filesss,666)
+		if err != nil{
+			log.Fatal(err)
+		}
+	}
+}
